b09c9e839d64f79aa4ee5e1f4ee754224b3fdcbe
hgunduz295 <36344949+hgunduz295@users.noreply.github.com>
2018-11-10 19:47:25 +0300

    Update Android.mk

--- recovery/Android.mk
+++ recovery/Android.mk
@@ -3,6 +3,7 @@ LOCAL_PATH := $(call my-dir)
 include $(CLEAR_VARS)
 LOCAL_C_INCLUDES := $(call project-path-for,recovery)
 LOCAL_SRC_FILES := recovery_updater.cpp
+LOCAL_VENDOR_MODULE := true
 LOCAL_MODULE := librecovery_updater_cm
 LOCAL_MODULE_TAGS := eng
 include $(BUILD_STATIC_LIBRARY)
